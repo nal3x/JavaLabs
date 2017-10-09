592f4cfdd34d68a4a81487d9b9b564e8e141fba7
nalex <nalex@e7450>
2017-10-09 03:54:58 +0300

    added Identity class

---
+++ src/Identity.java
@@ -0,0 +1,25 @@
+public class Identity {
+    
+   public static void main(String[] args)   {   
+       
+     	System.out.println("My name is ");    
+	System.out.println(args[0] + " " + args[1]);     
+     	System.out.println(" and i am ");    
+	System.out.println(args[2]);     
+	System.out.println(" years old.");   
+        
+//        if (args.length == 3)     
+//	    	{     
+//     	System.out.print("My name is ");    
+//     	System.out.print(args[0] + " " + args[1]);     
+//	System.out.print(" and i am ");    
+//	System.out.print(args[2]);     
+//	System.out.println(" years old.");    
+//	    	}     
+//	else
+//		{
+//		System.out.println("Missing parameters");
+//		}
+
+	}     
+}
