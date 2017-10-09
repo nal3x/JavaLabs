805950760301ac639412b42f3fc5bf95b40ea756
nalex <nalex@e7450>
2017-10-09 12:05:34 +0300

    EvenOdd empty

---
+++ src/EvenOddCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EvenOddCalculator {   
+    public static void main(String args[])  {    
+      
+    
+    }
+}
+
+/*Mod: τροποποιήστε ώστε οι αριθμοί να διαβάζονται μια φορά από την είσοδο. 
+Ο υπολογισμός πρέπει να σταματά όταν δεν υπάρχουν πλέον tokens σε αυτή.
+*/
