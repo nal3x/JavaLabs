78a04e50c3742f18a47067eb739fbc859ca72c2a
nalex <nalex@e7450>
2017-10-09 11:19:14 +0300

    Capitalize mods

--- src/Capitalize.java
+++ src/Capitalize.java
@@ -5,11 +5,15 @@ public class Capitalize {
     public static void main(String[] args) {
 
       Scanner input = new Scanner(System.in);
-
-      System.out.print("Enter your sentence: ");
+      System.out.print("Enter your sentence: ");  
+      System.out.println(input.nextLine().toUpperCase());
+          
       
-      String sentence = input.nextLine();
-     
-      System.out.println(sentence.toUpperCase());
     }
 }
+
+/*Mod 1: make each word of the capitalized sentence appear line by line
+  
+  Mod 2: scan for a specific word on the input and discard it on your output 
+         or substitute it with equal number of # characters 
+*/
